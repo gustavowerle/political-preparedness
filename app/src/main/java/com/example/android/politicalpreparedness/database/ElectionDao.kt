e883e629642b2b3ab23dacc3946069f5fa9eb05f
GustavoWerle01 <gustavo.l.werle@gmail.com>
2021-09-11 10:09:15 -0300

    Implement ElectionDao

--- app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -1,22 +1,28 @@
 package com.example.android.politicalpreparedness.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Update
 import com.example.android.politicalpreparedness.network.models.Election
 
 @Dao
 interface ElectionDao {
 
-    //TODO: Add insert query
+    @Insert
+    fun insertElection(election: Election)
 
-    //TODO: Add select all election query
+    @Query("SELECT * FROM election_table")
+    fun getAllElections(): LiveData<List<Election>>
 
-    //TODO: Add select single election query
+    @Query("SELECT * FROM election_table WHERE id = :id")
+    fun getElections(id: Int): Election
 
-    //TODO: Add delete query
+    @Delete
+    fun deleteElection(election: Election)
 
-    //TODO: Add clear query
+    @Query("DELETE FROM election_table")
+    fun clearElections()
 
 }
