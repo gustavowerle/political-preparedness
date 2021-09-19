b1cbbe8a5f17706c9641593e0862a831bbac0d48
GustavoWerle01 <gustavo.l.werle@gmail.com>
2021-09-19 10:11:51 -0300

    Fix dao issue

--- app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -1,6 +1,6 @@
 package com.example.android.politicalpreparedness.database
 
-import androidx.lifecycle.LiveData
+import androidx.room.OnConflictStrategy
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
@@ -10,19 +10,19 @@ import com.example.android.politicalpreparedness.network.models.Election
 @Dao
 interface ElectionDao {
 
-    @Insert
-    fun insertElection(election: Election)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertElection(election: Election)
 
     @Query("SELECT * FROM election_table")
-    fun getAllElections(): LiveData<List<Election>>
+    suspend fun getAllElections(): List<Election>
 
     @Query("SELECT * FROM election_table WHERE id = :id")
-    fun getElections(id: Int): Election
+    suspend fun getElections(id: Int): Election
 
     @Delete
-    fun deleteElection(election: Election)
+    suspend fun deleteElection(election: Election)
 
     @Query("DELETE FROM election_table")
-    fun clearElections()
+    suspend fun clearElections()
 
 }
