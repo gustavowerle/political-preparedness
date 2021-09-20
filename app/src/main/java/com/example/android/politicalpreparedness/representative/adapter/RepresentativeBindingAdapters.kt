2d87c85e5f5c4f8efed584f9d7b8054b654faad6
GustavoWerle01 <gustavo.l.werle@gmail.com>
2021-09-20 11:03:56 -0300

    Implement glide to download profile images

--- app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeBindingAdapters.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeBindingAdapters.kt
@@ -5,12 +5,19 @@ import android.widget.ImageView
 import android.widget.Spinner
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.example.android.politicalpreparedness.R
 
 @BindingAdapter("profileImage")
 fun fetchImage(view: ImageView, src: String?) {
     src?.let {
         val uri = src.toUri().buildUpon().scheme("https").build()
-        //TODO: Add Glide call to load image and circle crop - user ic_profile as a placeholder and for errors.
+        Glide.with(view.context)
+            .load(uri)
+            .placeholder(R.drawable.ic_profile)
+            .error(R.drawable.ic_profile)
+            .circleCrop()
+            .into(view)
     }
 }
 
@@ -26,6 +33,6 @@ fun Spinner.setNewValue(value: String?) {
     }
 }
 
-inline fun <reified T> toTypedAdapter(adapter: ArrayAdapter<*>): ArrayAdapter<T>{
+inline fun <reified T> toTypedAdapter(adapter: ArrayAdapter<*>): ArrayAdapter<T> {
     return adapter as ArrayAdapter<T>
 }
