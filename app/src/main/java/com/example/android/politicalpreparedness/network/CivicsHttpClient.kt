657a9155ebcc03c9378435c9a39d3435bd544169
GustavoWerle01 <gustavo.l.werle@gmail.com>
2021-09-20 10:57:10 -0300

    Fix secure issue with api key

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -1,14 +1,12 @@
 package com.example.android.politicalpreparedness.network
 
+import com.example.android.politicalpreparedness.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 
 class CivicsHttpClient : OkHttpClient() {
 
     companion object {
-
-        const val API_KEY = "AIzaSyC57et-rMgrrp97n6h2JZ1zI4RBEinPXEo" // TODO: DO NOT COMMIT, FIX SECURE ISSUE
-
         val interceptor = HttpLoggingInterceptor()
             .apply {
                 level = HttpLoggingInterceptor.Level.BODY
@@ -21,7 +19,7 @@ class CivicsHttpClient : OkHttpClient() {
                     val url = original
                         .url
                         .newBuilder()
-                        .addQueryParameter("key", API_KEY)
+                        .addQueryParameter("key", BuildConfig.API_KEY)
                         .build()
                     val request = original
                         .newBuilder()
